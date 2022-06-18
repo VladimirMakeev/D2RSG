f88ee22bf9a01e82c95c4dea56d582b6b2679e95
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-06-19 02:12:12 +0300

    Add Position methods

--- position.h
+++ position.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+#include <cstdint>
 #include <ostream>
 
 // Represents point on a 2D-plane
@@ -11,6 +13,50 @@ struct Position
         : x{x}
         , y{y}
     { }
+
+    Position& operator+=(const Position& other)
+    {
+        x += other.x;
+        y += other.y;
+
+        return *this;
+    }
+
+    bool operator<(const Position& other) const
+    {
+        if (y < other.y) {
+            return true;
+        }
+
+        if (y > other.y) {
+            return false;
+        }
+
+        if (x < other.x) {
+            return true;
+        }
+
+        if (x > other.x) {
+            return false;
+        }
+
+        return false;
+    }
+
+    // Returns squared distance
+    std::uint32_t distanceSquared(const Position& other) const
+    {
+        const auto dx{x - other.x};
+        const auto dy{y - other.y};
+
+        return static_cast<std::uint32_t>(dx * dx) + static_cast<std::uint32_t>(dy * dy);
+    }
+
+    // Returns distance
+    double distance(const Position& other) const
+    {
+        return std::sqrt(static_cast<double>(distanceSquared(other)));
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const Position& p)
     {
