00d9a2b051dc0b0d606b27422f5a3938ffa8a9ad
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-06-23 20:48:09 +0300

    Update MapGenerator code

--- mapgenerator.cpp
+++ mapgenerator.cpp
@@ -1,8 +1,40 @@
 #include "mapgenerator.h"
+#include "fog.h"
+#include "knownspells.h"
 #include "maptemplate.h"
+#include "player.h"
+#include "playerbuildings.h"
+#include <cassert>
 #include <iostream>
+#include <numeric>
 #include <sstream>
 #include <stdexcept>
+
+CMidgardID MapGenerator::createPlayer(RaceType race)
+{
+    auto playerId{createId(CMidgardID::Type::Player)};
+
+    auto player{std::make_unique<Player>(playerId)};
+    player->setRace(getRaceId(race));
+    player->setLord(getLordId(race));
+
+    // Create fog, knownSpells and buildings.
+    // They are necessary for each player
+    auto fogId{createId(CMidgardID::Type::Fog)};
+    player->setFogId(fogId);
+    insertObject(std::make_unique<Fog>(fogId));
+
+    auto buildingsId{createId(CMidgardID::Type::PlayerBuildings)};
+    player->setBuildingsId(buildingsId);
+    insertObject(std::make_unique<PlayerBuildings>(buildingsId));
+
+    auto spellsId{createId(CMidgardID::Type::PlayerKnownSpells)};
+    player->setSpellsId(spellsId);
+    insertObject(std::make_unique<KnownSpells>(spellsId));
+
+    insertObject(std::move(player));
+    return playerId;
+}
 
 MapPtr MapGenerator::generate()
 {
@@ -10,9 +42,13 @@ MapPtr MapGenerator::generate()
 
     addHeaderInfo();
     initTiles();
+
+    // Create neutral player first
+    neutralPlayerId = createPlayer(RaceType::Neutral);
+
     generateZones();
     // Clear map so that all tiles are unguarded
-    // map->calculateGuardingCreaturePositions();
+    map->calculateGuardingCreaturePositions();
     fillZones();
 
     return std::move(map);
@@ -36,7 +72,6 @@ void MapGenerator::initTiles()
 
 void MapGenerator::generateZones()
 {
-    // TODO: create template instance
     auto tmpl = mapGenOptions.mapTemplate;
 
     zones.clear();
@@ -57,16 +92,146 @@ void MapGenerator::generateZones()
 }
 
 void MapGenerator::fillZones()
+{
+    std::cout << "Started filling zones\n";
+
+    const auto& races{mapGenOptions.mapTemplate->races};
+    std::size_t raceIndex{};
+
+    // Create players, assign player id to each starting zone
+    for (auto& it : zones) {
+        auto& zone{it.second};
+
+        if (zone->type == TemplateZoneType::PlayerStart
+            || zone->type == TemplateZoneType::AiStart) {
+            auto race{getNextRace(raceIndex)};
+            assert(race != RaceType::Neutral);
+
+            auto playerId{createPlayer(race)};
+            zone->setOwner(playerId);
+        }
+    }
+
+    for (auto& it : zones) {
+        it.second->initTowns();
+    }
+
+    // Make sure there are some free tiles in the zone
+    for (auto& it : zones) {
+        it.second->initFreeTiles();
+    }
+
+    for (auto& it : zones) {
+        it.second->createBorder();
+    }
+
+    createDirectConnections();
+
+    for (auto& it : zones) {
+        it.second->fill();
+    }
+
+    createObstacles();
+
+    // Place actual obstacles matching zone terrain
+    for (auto& it : zones) {
+        it.second->createObstacles();
+    }
+
+    for (auto& it : zones) {
+        it.second->connectRoads();
+    }
+}
+
+void MapGenerator::createDirectConnections()
+{
+    for (auto& connection : mapGenOptions.mapTemplate->connections) {
+        auto zoneA{zones[connection.zoneA]};
+        auto zoneB{zones[connection.zoneB]};
+
+        const auto zoneBId{zoneB->id};
+        Position guardPos{-1, -1};
+
+        std::vector<Position> middleTiles{};
+        for (auto& tile : zoneA->getTileInfo()) {
+            if (isUsed(tile)) {
+                continue;
+            }
+
+            // Must be direct since paths also generated between direct neighbours
+            foreachDirectNeighbor(tile, [this, &tile, &middleTiles, zoneBId](Position& position) {
+                if (getZoneId(position) == zoneBId) {
+                    middleTiles.push_back(tile);
+                }
+            });
+        }
+
+        // Find tiles with minimum manhattan distance from center of the mass of zone border
+        const auto tilesCount{middleTiles.empty() ? std::size_t{1} : middleTiles.size()};
+
+        auto middleTile{std::accumulate(middleTiles.begin(), middleTiles.end(), Position(0, 0))};
+        middleTile /= tilesCount;
+
+        std::sort(middleTiles.begin(), middleTiles.end(),
+                  [&middleTile](const Position& a, Position& b) {
+                      // Choose tiles with both corrdinates in the middle
+                      return a.mahnattanDistance(middleTile) < b.mahnattanDistance(middleTile);
+                  });
+
+        // Remove 1/4 tiles from each side - path should cross zone borders at smooth angle
+        const auto removeCount{tilesCount / 4};
+
+        middleTiles.erase(middleTiles.end() - removeCount, middleTiles.end());
+        middleTiles.erase(middleTiles.begin(), middleTiles.begin() + removeCount);
+
+        randomShuffle(middleTiles, randomGenerator);
+
+        for (auto& tile : middleTiles) {
+            guardPos = tile;
+
+            if (guardPos.isValid()) {
+                // Zones can make paths only in their own area
+                zoneA->connectWithCenter(guardPos, true, true);
+                zoneB->connectWithCenter(guardPos, true, true);
+
+                const auto exists{zoneA->addStack(guardPos, connection.guardStrength, false, true)};
+                // Place next objects away from guard in both zones
+                zoneB->updateDistances(guardPos);
+
+                // Set free tile only after connection is made to the center of the zone
+                if (!exists) {
+                    // Strength is too weak for guard to spawn
+                    setOccupied(guardPos, TileType::Free);
+                }
+
+                zoneA->addRoadNode(guardPos);
+                zoneB->addRoadNode(guardPos);
+                break;
+            }
+        }
+
+        // TODO: check water zones, update 'connectionsLeft' ?
+    }
+}
+
+void MapGenerator::createObstacles()
 { }
+
+TemplateZoneId MapGenerator::getZoneId(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return zoneColoring[posToIndex(position)];
+}
 
 void MapGenerator::setZoneId(const Position& position, TemplateZoneId zoneId)
 {
-    checkInOnMap(position);
+    checkIsOnMap(position);
 
     zoneColoring[posToIndex(position)] = zoneId;
 }
 
-void MapGenerator::checkInOnMap(const Position& position)
+void MapGenerator::checkIsOnMap(const Position& position) const
 {
     if (!map->isInTheMap(position)) {
         std::stringstream stream;
@@ -74,4 +239,194 @@ void MapGenerator::checkInOnMap(const Position& position)
 
         throw std::runtime_error(stream.str());
     }
+}
+
+bool MapGenerator::isBlocked(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].isBlocked();
+}
+
+bool MapGenerator::shouldBeBlocked(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].shouldBeBlocked();
+}
+
+bool MapGenerator::isPossible(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].isPossible();
+}
+
+bool MapGenerator::isFree(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].isFree();
+}
+
+bool MapGenerator::isUsed(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].isUsed();
+}
+
+bool MapGenerator::isRoad(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].isRoad();
+}
+
+void MapGenerator::setOccupied(const Position& position, TileType value)
+{
+    checkIsOnMap(position);
+
+    tiles[posToIndex(position)].setOccupied(value);
+}
+
+void MapGenerator::setRoad(const Position& position, bool value)
+{
+    checkIsOnMap(position);
+
+    tiles[posToIndex(position)].setRoad(value);
+}
+
+void MapGenerator::foreachNeighbor(const Position& position, std::function<void(Position&)> f)
+{
+    for (const auto& direction : Position::getDirections()) {
+        Position p{position + direction};
+
+        if (map->isInTheMap(p)) {
+            f(p);
+        }
+    }
+}
+
+void MapGenerator::foreachDirectNeighbor(const Position& position, std::function<void(Position&)> f)
+{
+    // clang-format off
+    static const std::array<Position, 4> directions{{
+        Position{ 0, -1},
+        Position{ 0,  1},
+        Position{-1, 0},
+        Position{ 1, 0}
+    }};
+    // clang-format on
+
+    for (const auto& direction : directions) {
+        Position p{position + direction};
+
+        if (map->isInTheMap(p)) {
+            f(p);
+        }
+    }
+}
+
+float MapGenerator::getNearestObjectDistance(const Position& position) const
+{
+    checkIsOnMap(position);
+
+    return tiles[posToIndex(position)].getNearestObjectDistance();
+}
+
+void MapGenerator::setNearestObjectDistance(const Position& position, float value)
+{
+    checkIsOnMap(position);
+
+    tiles[posToIndex(position)].setNearestObjectDistance(value);
+}
+
+const CMidgardID& MapGenerator::getRaceId(RaceType race) const
+{
+    // Values from GRace.dbf
+    // clang-format off
+    static const std::array<CMidgardID, (size_t)RaceType::Total> raceIds{{
+        CMidgardID{"g000rr0000"},
+        CMidgardID{"g000rr0003"},
+        CMidgardID{"g000rr0002"},
+        CMidgardID{"g000rr0001"},
+        CMidgardID{"g000rr0004"},
+        CMidgardID{"g000rr0005"}
+    }};
+    // clang-format on
+
+    return raceIds[static_cast<std::size_t>(race)];
+}
+
+const CMidgardID& MapGenerator::getLordId(RaceType race) const
+{
+    // We don't care about actual lord id, since player will choose it at the start of scenario.
+    // These ids are for errorless and convenient map generation
+
+    // clang-format off
+    static const std::array<CMidgardID, (size_t)RaceType::Total> lordIds{{
+        CMidgardID{"g000LR0001"},
+        CMidgardID{"g000LR0010"},
+        CMidgardID{"g000LR0007"},
+        CMidgardID{"g000LR0004"},
+        CMidgardID{"g000LR0013"},
+        CMidgardID{"g000LR0016"}
+    }};
+    // clang-format on
+
+    return lordIds[static_cast<std::size_t>(race)];
+}
+
+RaceType MapGenerator::getRaceType(const CMidgardID& raceId) const
+{
+    assert(raceId.getCategory() == CMidgardID::Category::Global
+           && raceId.getType() == CMidgardID::Type::Race);
+
+    switch (raceId.getTypeIndex()) {
+    case 0:
+        return RaceType::Human;
+    case 1:
+        return RaceType::Dwarf;
+    case 2:
+        return RaceType::Heretic;
+    case 3:
+        return RaceType::Undead;
+    default:
+    case 4:
+        return RaceType::Neutral;
+    case 5:
+        return RaceType::Elf;
+    }
+}
+
+void MapGenerator::registerZone(RaceType race)
+{
+    zonesPerRace[race]++;
+    zonesTotal++;
+}
+
+std::size_t MapGenerator::getZoneCount(RaceType race)
+{
+    return zonesPerRace[race];
+}
+
+std::size_t MapGenerator::getTotalZoneCount() const
+{
+    return zonesTotal;
+}
+
+RaceType MapGenerator::getNextRace(std::size_t& raceIndex) const
+{
+    const auto& races{mapGenOptions.mapTemplate->races};
+
+    for (; raceIndex < races.size();) {
+        auto index{raceIndex++};
+        // Neutrals always exist, and we are not interested in them
+        if (races[index] && static_cast<RaceType>(index) != RaceType::Neutral) {
+            return static_cast<RaceType>(index);
+        }
+    }
+
+    return RaceType::Neutral;
 }
--- mapgenerator.h
+++ mapgenerator.h
@@ -4,6 +4,7 @@
 #include "randomgenerator.h"
 #include "tileinfo.h"
 #include "zoneplacer.h"
+#include <functional>
 #include <vector>
 
 struct MapTemplate;
@@ -26,8 +27,20 @@ public:
         : mapGenOptions{mapGenOptions}
         , randomSeed{randomSeed}
     {
-        randomGenerator.setSeed(randomSeed);
+        randomGenerator.setSeed(static_cast<std::size_t>(randomSeed));
     }
+
+    CMidgardID createId(CMidgardID::Type type)
+    {
+        return map->createId(type);
+    }
+
+    bool insertObject(std::unique_ptr<ScenarioObject>&& object)
+    {
+        return map->insertObject(std::move(object));
+    }
+
+    CMidgardID createPlayer(RaceType race);
 
     MapPtr generate();
 
@@ -35,9 +48,42 @@ public:
     void initTiles();
     void generateZones();
     void fillZones();
+    void createDirectConnections();
+    void createObstacles();
 
+    TemplateZoneId getZoneId(const Position& position) const;
     void setZoneId(const Position& position, TemplateZoneId zoneId);
-    void checkInOnMap(const Position& position);
+    void checkIsOnMap(const Position& position) const;
+
+    bool isBlocked(const Position& position) const;
+    bool shouldBeBlocked(const Position& position) const;
+    bool isPossible(const Position& position) const;
+    bool isFree(const Position& position) const;
+    bool isUsed(const Position& position) const;
+    bool isRoad(const Position& position) const;
+
+    void setOccupied(const Position& position, TileType value);
+    void setRoad(const Position& position, bool value);
+
+    void foreachNeighbor(const Position& position, std::function<void(Position&)> f);
+    void foreachDirectNeighbor(const Position& position, std::function<void(Position&)> f);
+
+    float getNearestObjectDistance(const Position& position) const;
+    void setNearestObjectDistance(const Position& position, float value);
+
+    // Returns global race id for specified race
+    const CMidgardID& getRaceId(RaceType race) const;
+    // Returns global lord id for specified race
+    const CMidgardID& getLordId(RaceType race) const;
+    // Returns race type for specified race id
+    RaceType getRaceType(const CMidgardID& raceId) const;
+
+    void registerZone(RaceType race);
+    std::size_t getZoneCount(RaceType race);
+    std::size_t getTotalZoneCount() const;
+
+    // Returns next playable race
+    RaceType getNextRace(std::size_t& raceIndex) const;
 
     std::size_t posToIndex(const Position& position) const
     {
@@ -47,8 +93,11 @@ public:
     std::vector<TileInfo> tiles;
     std::vector<TemplateZoneId> zoneColoring;
     ZonesMap zones;
+    std::map<RaceType, std::size_t> zonesPerRace;
     MapPtr map;
     RandomGenerator randomGenerator;
     MapGenOptions& mapGenOptions;
     time_t randomSeed;
+    CMidgardID neutralPlayerId{emptyId};
+    std::size_t zonesTotal{}; // Zones with capital town only
 };
