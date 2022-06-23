348791dcdf1a2d83a0751ce230fd44a59e9e4e08
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-06-23 20:45:01 +0300

    Add utility Image class

---
+++ image.cpp
@@ -0,0 +1,13 @@
+#include "image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+bool Image::write(std::filesystem::path& file) const
+{
+    return write(file.string().c_str());
+}
+
+bool Image::write(const char* file) const
+{
+    return stbi_write_png(file, (int)width, (int)height, 3, (const void*)pixels.data(), 3 * width);
+}
---
+++ image.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
+#include <vector>
+
+struct RgbColor
+{
+    RgbColor() = default;
+
+    RgbColor(std::uint8_t r, std::uint8_t g, std::uint8_t b)
+        : r{r}
+        , g{g}
+        , b{b}
+    { }
+
+    std::uint8_t r{};
+    std::uint8_t g{};
+    std::uint8_t b{};
+};
+
+class Image
+{
+public:
+    Image() = delete;
+
+    Image(std::size_t width, std::size_t height, const std::vector<RgbColor>& pixels)
+        : pixels{pixels}
+        , width{width}
+        , height{height}
+    { }
+
+    bool write(std::filesystem::path& file) const;
+    bool write(const char* file) const;
+
+private:
+    std::vector<RgbColor> pixels;
+    std::size_t width;
+    std::size_t height;
+};
