81b57493a907428556f259f40903937c4043829a
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-06-26 04:32:09 +0300

    Add Plan class

---
+++ plan.cpp
@@ -0,0 +1,34 @@
+#include "plan.h"
+#include "map.h"
+#include "mapelement.h"
+#include "serializer.h"
+
+void Plan::serialize(Serializer& serializer, const Map& scenario) const
+{
+    serializer.enterRecord();
+
+    CMidgardID::String idString{};
+    objectId.toString(idString);
+
+    serializer.serialize(idString.data(), scenario.size);
+    serializer.serialize(idString.data(), static_cast<std::uint32_t>(entries.size()));
+
+    for (const auto& entry : entries) {
+        serializer.serialize("POS_X", "POS_Y", entry.position);
+        serializer.serialize("ELEMENT", entry.objectId);
+    }
+
+    serializer.leaveRecord();
+}
+
+void Plan::add(const MapElement& mapElement, const CMidgardID& mapElementId)
+{
+    const auto& position{mapElement.getPosition()};
+    const auto& size{mapElement.getSize()};
+
+    for (int x = 0; x < size.x; ++x) {
+        for (int y = 0; y < size.y; ++y) {
+            entries.push_back({mapElementId, {position.x + x, position.y + y}});
+        }
+    }
+}
---
+++ plan.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "position.h"
+#include "scenarioobject.h"
+#include <vector>
+
+class MapElement;
+
+class Plan : public ScenarioObject
+{
+public:
+    Plan(const CMidgardID& id)
+        : ScenarioObject(id)
+    { }
+
+    ~Plan() override = default;
+
+    const char* rawName() const override
+    {
+        return ".?AVCMidgardPlan@@";
+    }
+
+    void serialize(Serializer& serializer, const Map& scenario) const override;
+
+    void add(const MapElement& mapElement, const CMidgardID& mapElementId);
+
+private:
+    struct Entry
+    {
+        CMidgardID objectId;
+        Position position;
+    };
+
+    std::vector<Entry> entries;
+};
