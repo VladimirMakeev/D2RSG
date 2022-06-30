923fcd45b43d5ffc11dd0811c26a2658448790a4
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-06-30 20:34:11 +0300

    Add Crystal class

---
+++ crystal.cpp
@@ -0,0 +1,12 @@
+#include "crystal.h"
+#include "serializer.h"
+
+void Crystal::serialize(Serializer& serializer, const Map& scenario) const
+{
+    serializer.enterRecord();
+    serializer.serialize("CRYSTAL_ID", objectId);
+    serializer.serialize("RESOURCE", static_cast<std::uint32_t>(resourceType));
+    serializer.serialize("POS_X", "POS_Y", position);
+    serializer.serialize("AIPRIORITY", static_cast<std::uint32_t>(aiPriority.getPriority()));
+    serializer.leaveRecord();
+}
---
+++ crystal.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "aipriority.h"
+#include "enums.h"
+#include "mapelement.h"
+#include "scenarioobject.h"
+
+class Crystal
+    : public ScenarioObject
+    , public MapElement
+{
+public:
+    Crystal(const CMidgardID& crystalId)
+        : ScenarioObject(crystalId)
+        , MapElement(Position{1, 1})
+    { }
+
+    ~Crystal() override = default;
+
+    const char* rawName() const override
+    {
+        return ".?AVCMidCrystal@@";
+    }
+
+    void serialize(Serializer& serializer, const Map& scenario) const override;
+
+    ResourceType getResourceType() const
+    {
+        return resourceType;
+    }
+
+    void setResourceType(ResourceType value)
+    {
+        resourceType = value;
+    }
+
+private:
+    AiPriority aiPriority;
+    ResourceType resourceType{ResourceType::Gold};
+};
