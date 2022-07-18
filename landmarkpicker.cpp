a57e51630bc60a5c9b0c9fd0fa6cf179fa3b5bec
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-07-18 19:37:03 +0300

    Add landmark picking functions

---
+++ landmarkpicker.cpp
@@ -0,0 +1,39 @@
+#include "landmarkpicker.h"
+#include "gameinfo.h"
+#include "randomgenerator.h"
+
+LandmarkInfo* pickLandmark(const std::vector<LandmarkInfo*>& landmarkPool,
+                           Rng& random,
+                           const LandmarkFilterList& filters)
+{
+    LandmarkInfoArray pool{landmarkPool};
+
+    for (auto& filter : filters) {
+        pool.erase(std::remove_if(pool.begin(), pool.end(), filter), pool.end());
+    }
+
+    if (pool.empty()) {
+        // Filters are too tight, nothing to pick
+        return nullptr;
+    }
+
+    const auto index{static_cast<std::size_t>(random.getInt64Range(0, pool.size() - 1)())};
+    return pool[index];
+}
+
+LandmarkInfo* pickLandmark(LandmarkType landmarkType,
+                           Rng& random,
+                           const LandmarkFilterList& filters)
+{
+    return pickLandmark(getLandmarks(landmarkType), random, filters);
+}
+
+LandmarkInfo* pickLandmark(RaceType raceType, Rng& random, const LandmarkFilterList& filters)
+{
+    return pickLandmark(getLandmarks(raceType), random, filters);
+}
+
+LandmarkInfo* pickMountainLandmark(Rng& random, const LandmarkFilterList& filters)
+{
+    return pickLandmark(getMountainLandmarks(), random, filters);
+}
---
+++ landmarkpicker.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "enums.h"
+#include <functional>
+#include <vector>
+
+struct LandmarkInfo;
+class Rng;
+
+// Filter that decides whether landmark should be discarded from pick or not.
+// Returns true for landmark that should be removed from pick pool
+using LandmarkFilterFunc = std::function<bool(const LandmarkInfo*)>;
+using LandmarkFilterList = std::initializer_list<LandmarkFilterFunc>;
+
+// Pick random landmark from specified pool
+LandmarkInfo* pickLandmark(const std::vector<LandmarkInfo*>& landmarkPool,
+                           Rng& random,
+                           const LandmarkFilterList& filters);
+// Pick random landmark of specified type
+LandmarkInfo* pickLandmark(LandmarkType landmarkType,
+                           Rng& random,
+                           const LandmarkFilterList& filters);
+// Pick random landmark visually appropriate for specified race
+LandmarkInfo* pickLandmark(RaceType raceType, Rng& random, const LandmarkFilterList& filters);
+// Pick random mountain landmark
+LandmarkInfo* pickMountainLandmark(Rng& random, const LandmarkFilterList& filters);
