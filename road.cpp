35048cf5c87461977d7ac592c5d358ba4b4bdb11
VladimirMakeev <VladimirMakeev@users.noreply.github.com>
2022-12-30 21:35:39 +0300

    Serialize all road and village fields for easier .sg files debugging

--- road.cpp
+++ road.cpp
@@ -7,8 +7,10 @@ void Road::serialize(Serializer& serializer, const Map& scenario) const
     serializer.serialize("ROAD_ID", objectId);
     serializer.serialize("INDEX", index);
     // According to game code, roads can be without 'VAR' field.
-    // Save some bytes and do not serialize it, since it is always (?) zero
-    // serializer.serialize("VAR", 0);
+    // We can save some bytes and do not serialize it, since it is always (?) zero.
+    // My Kaitai Struct .sg format description does not handle roads without 'VAR' field,
+    // so leave it as is for now to be able to debug generated scenarios.
+    serializer.serialize("VAR", 0);
     serializer.serialize("POS_X", "POS_Y", position);
     serializer.leaveRecord();
 }
--- village.cpp
+++ village.cpp
@@ -15,8 +15,10 @@ void Village::serialize(Serializer& serializer, const Map& scenario) const
     serializer.serialize("P_O_HU", false);
     serializer.serialize("P_O_DW", false);
     // According to game code, villages can be without 'P_O_EL' field.
-    // Save some bytes and do not serialize it
-    // serializer.serialize("P_O_EL", false);
+    // We can save some bytes and do not serialize it.
+    // My Kaitai Struct .sg format description does not handle villages without 'P_O_EL' field,
+    // so leave it as is for now to be able to debug generated scenarios.
+    serializer.serialize("P_O_EL", false);
     serializer.serialize("RIOT_T", riotTurn);
     serializer.leaveRecord();
 }
